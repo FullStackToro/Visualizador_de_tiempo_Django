fd82bbbde6a9cf4bf1e5bb6b35649852e96694dd
FullStackToro <g.toro.guillon@gmail.com>
2021-01-12 22:44:43 -0300

    commit inicial

---
+++ time_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, HttpResponse
+from time import gmtime, strftime
+from datetime import datetime
+
+
+def index(request):
+    return HttpResponse("aca va la ruta")
+
+def tiempo(request):
+    now = datetime.now()
+    mes=['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sept', 'Oct', 'Nov', 'Dic']
+    mes_final=mes[now.month-1]
+    context = {
+        "time": strftime("%Y-%m-%d %H:%M %p", gmtime()),
+        "fecha": strftime("%b %d, %Y", gmtime()),
+        "hora": strftime("%H:%M %p", gmtime()),
+        "fecha2": str(f" {mes_final} {now.day},  {now.year}"),
+        "hora2": str(f"{now.hour}:{now.minute}"),
+    }
+    return render(request, 'index.html', context)
